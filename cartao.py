77a024ef3515bfa7e63189ea587530b03e0b3211
MarcellaCalcado <55627854+MarcellaCalcado@users.noreply.github.com>
2026-09-10 17:20:29 -0300

    Criar cartao.py

---
+++ cartao.py
@@ -0,0 +1,5 @@
+nome = input ("Digite seu nome:")
+idade = input ("Digite sua idade:")
+cidade = input ("Digite sua cidade:")
+
+print(f"Olá, meu nome é {nome}, eu tenho {idade} anos e moro em {cidade}")
